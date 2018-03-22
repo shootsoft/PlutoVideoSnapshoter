ad006e94087415c5920d6fc80720667c921496a4
shootsoft <shootsoft@qq.com>
2018-03-22 22:17:31 +1100

    Update setup file

--- setup.py
+++ setup.py
@@ -5,14 +5,34 @@ import sys
 from setuptools import setup
 
 APP = ['src/app.py']
-DATA_FILES = [('windows', ['src/windows/image_stitching_window.ui'])]
+DATA_FILES = [
+    ('windows', ['src/windows/image_stitching_window.ui',
+                 'src/windows/video_player_window.ui',
+                 'src/windows/pluto.png'
+                 ]),
+    ('', ['res/qt.conf'])
+]
 OPTIONS = {
     'argv_emulation': True,
     'iconfile': 'res/pluto.icns',
     'plist': {'CFBundleShortVersionString': '1.1', },
-    'includes': ['sip', 'opencv-python', 'numpy', 'Pillow',
-                 'PyQt5', 'PyQt5.QtCore', 'PyQt5.QtGui', 'PyQt5.QtWidgets', 'PyQt5.QtMultimedia',
-                 'PyQt5.QtMultimediaWidgets'],
+    'includes': ['sip',
+                 'opencv-python',
+                 'numpy',
+                 'Pillow',
+                 'PyQt5',
+                 'PyQt5.QtCore',
+                 'PyQt5.QtGui',
+                 'PyQt5.QtWidgets',
+                 'PyQt5.QtMultimedia',
+                 'PyQt5.QtMultimediaWidgets',
+                 'PyQt5.pyqtconfig',
+                 'pluto.*',
+                 'pluto.media.*',
+                 'pluto.video.*',
+                 'pluto.video.ui.*',
+                 'pluto.video.ui.mvc.*'
+                 ],
     'bdist_base': os.path.join(os.getcwd(), 'build', 'build'),
     'dist_dir': os.path.join(os.getcwd(), 'build', 'dist'),
 }
