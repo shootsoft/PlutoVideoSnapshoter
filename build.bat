374fb798c05f8e1caf02722ed07a5b6873c2b4d7
shootsoft <shootsoft@qq.com>
2018-03-25 19:34:21 +1100

    Update build command for windows

--- build.bat
+++ build.bat
@@ -1,7 +1,20 @@
-pyinstaller -F -w --icon=res/pluto.ico --add-data "src\windows;windows" --add-data "venv/v36/Lib/site-packages/cv2/opencv_ffmpeg340.dll;./"  --path "C:\Program Files (x86)\Windows Kits\10\Redist\ucrt\DLLs\x86" --version-file=file_version_info.txt "src\app.py"
+pyinstaller -F -w --icon=res/pluto.ico ^
+          --add-data "src\windows;windows" ^
+          --add-data "venv/v36/Lib/site-packages/cv2/opencv_ffmpeg340.dll;./"  ^
+          --path "C:\Program Files (x86)\Windows Kits\10\Redist\ucrt\DLLs\x86" ^
+          --version-file=file_version_info.txt "src\app.py" ^
+          -n "PlutoVideoSnapshoter"
 
+goto comment
+...skip this...
 REM Multiple files
-REM pyinstaller --clean -D -w --icon=res/pluto.ico --add-data "src\windows;windows" --add-data "venv/v36/Lib/site-packages/cv2/opencv_ffmpeg340.dll;./"  --path "C:\Program Files (x86)\Windows Kits\10\Redist\ucrt\DLLs\x86" --version-file=file_version_info.txt "src\app.py"
+pyinstaller -D -w --icon=res/pluto.ico ^
+          --add-data "src\windows;windows" ^
+          --add-data "venv/v36/Lib/site-packages/cv2/opencv_ffmpeg340.dll;./"  ^
+          --path "C:\Program Files (x86)\Windows Kits\10\Redist\ucrt\DLLs\x86" ^
+          --version-file=file_version_info.txt "src\app.py" ^
+          -n "PlutoVideoSnapshoter"
 
 REM Mac
 REM pyinstaller --clean -D -w --icon=res/pluto.icns --add-data "src/windows:windows"  "src/app.py" -n "PlutoVideoSnapshoter"
+:comment
